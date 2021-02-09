f58df96bad41429cdd5f90c45cbde2c06d5f537e
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-02-09 23:54:33 +0300

    The second homework

---
+++ hw_02/HW.02.Image/HW.02.Casting/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HW._02.Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Явные преобразования
+            int a = 2;
+            int b = 3;
+            byte c = (byte)(a + b);
+
+            short s = 45;
+            byte b1 = (byte)s; 
+            
+            double d= 123;
+            int dint = (int)d;
+
+
+            //Неявное преобразование
+            byte n = 5;
+            short s1 = n;
+            long l = 12232423;
+            float f3 = l;
+            double d1 = f3;
+
+            //boxing
+            object o1 = new object();
+            object o2 = new object();
+            object o3 = new object();
+            o1 = a;
+            o2 = b;
+            o3 = d;
+
+            //unboxing
+            int a_new, b_new; double d_new;
+            a_new = (int)o1;
+            b_new = (int)o2;
+            d_new = (double)o3;
+
+
+        }
+    }
+}
---
+++ hw_02/HW.02.Image/HW.02.Types/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace HW._02.Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int code_a = 1;
+            System.Int32 clr_a = 2;
+            Console.WriteLine(code_a.GetType() + " = " + clr_a.GetType());
+
+            byte code_b = 3;
+            System.Byte clr_b = 4;
+            Console.WriteLine(code_b.GetType() + " = " + clr_b.GetType());
+
+            char code_c = 'a';
+            System.Char clr_c = 'b';
+            Console.WriteLine(code_c.GetType() + " = " + clr_c.GetType());
+
+            double code_d = 1.8374;
+            System.Double clr_d = 3.14;
+            Console.WriteLine(code_d.GetType() + " = " + clr_d.GetType());
+
+            float code_f = 1.22f;
+            System.Single clr_f = 1.2245f;
+            Console.WriteLine(code_f.GetType() + " = " + clr_f.GetType());
+
+            object code_o1 = new object();
+            System.Object clr_o1 = new System.Object();
+            Console.WriteLine(code_o1.GetType() + " = " + clr_o1.GetType());
+
+            string code_string = "djfjhhfdf";
+            System.String clr_string = "dffdgkopkpog";
+            Console.WriteLine(code_string.GetType() + " = " + clr_string.GetType());
+
+            Console.ReadKey();
+        }
+    }
+}
