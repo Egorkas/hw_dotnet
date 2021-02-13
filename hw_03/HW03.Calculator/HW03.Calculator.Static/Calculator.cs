3da83832cea471651dad5b6f2e304742a761a2b0
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-02-14 00:55:52 +0300

    Add Calcultor.Static, fix DivideByZeroException in int

---
+++ hw_03/HW03.Calculator/HW03.Calculator.Static/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW03.Calculator.Static
+{
+    class Calculator
+    {
+        public static double Addition(double firstValue, double secondValue) => firstValue + secondValue;
+        public static double Subtraction(double firstValue, double secondValue) => firstValue - secondValue;
+        public static double Multiplication(double firstValue, double secondValue) => firstValue * secondValue;
+        public static int Division(int firstValue, int secondValue)
+        {
+            try
+            {
+                return firstValue / secondValue;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Enter the secondValue another time");
+                int secondValue2 = Int32.Parse(Console.ReadLine());
+                return Division(firstValue, secondValue2);
+            }
+        }
+        public static double Remainder(double firstValue, double secondValue) => firstValue % secondValue;
+        public static double AreaOfCircle(double radius) => radius * radius * Math.PI;
+    }
+}
---
+++ hw_03/HW03.Calculator/HW03.Calculator.Static/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+
+namespace HW03.Calculator.Static
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Call Addition            
+            Console.Write("Input first value: ");
+            double firstValue1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input second value: ");
+            double secondValue1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Value = {Calculator.Addition(firstValue1, secondValue1)}");
+
+            //Call Substraction           
+            Console.Write("Input first value: ");
+            double firstValue2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input second value: ");
+            double secondValue2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Value = {Calculator.Subtraction(firstValue2, secondValue2)}");
+
+            //Call Multiplication           
+            Console.Write("Input first value: ");
+            double firstValue3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input second value: ");
+            double secondValue3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Value = {Calculator.Multiplication(firstValue3, secondValue3)}");
+
+            //Call Division(use Int32)           
+            Console.Write("Input first value: ");
+            int firstValue4 = Int32.Parse(Console.ReadLine());
+            Console.Write("Input second value: ");
+            int secondValue4 = Int32.Parse(Console.ReadLine());
+            Console.WriteLine($"Value = {Calculator.Division(firstValue4, secondValue4)}");
+
+            //Call Remainder            
+            Console.Write("Input first value: ");
+            double firstValue5 = double.Parse(Console.ReadLine());
+            Console.Write("Input second value: ");
+            double secondValue5 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Remainder = {Calculator.Remainder(firstValue5, secondValue5)}");
+
+            //Call AreaOfCircle            
+            Console.Write("Input radius: ");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Area of circle = {Calculator.AreaOfCircle(radius)}");
+        }
+    }
+}
