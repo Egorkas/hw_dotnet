c9ebba3bb0e3a782edde1ac8eebbb57bb230a2a5
Egorkas <37396590+Egorkas@users.noreply.github.com>
2021-02-13 23:57:46 +0300

    The third homework

---
+++ hw_03/HW03.Calculator/HW03.Calculator/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HW03.Calculator
+{
+    class Calculator
+    {
+        public double Addition(double firstValue, double secondValue) => firstValue + secondValue;
+        public double Subtraction(double firstValue, double secondValue) => firstValue - secondValue;
+        public double Multiplication(double firstValue, double secondValue) => firstValue * secondValue;
+        public double Division(double firstValue, double secondValue) => firstValue / secondValue;
+        //{
+        //    try
+        //    {
+        //        return firstValue / secondValue;
+        //    }
+        //    catch(DivideByZeroException ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //        Console.WriteLine("Enter the secondValue another time");
+        //        double secondValue2 = Int32.Parse(Console.ReadLine());
+        //        return Division(firstValue, secondValue2);
+        //    }
+        //}
+        public double Remainder(double firstValue, double secondValue) => firstValue % secondValue;
+        public double AreaOfCircle(double radius) => radius * radius * Math.PI;
+    }
+}
---
+++ hw_03/HW03.Calculator/HW03.Calculator/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace HW03.Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator calc = new Calculator();
+
+            //Call Addition            
+            Console.Write("Input first value: ");
+            double firstValue1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input second value: ");
+            double secondValue1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Value = {calc.Addition(firstValue1, secondValue1)}");            
+
+            //Call Substraction           
+            Console.Write("Input first value: ");
+            double firstValue2 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input second value: ");
+            double secondValue2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Value = {calc.Subtraction(firstValue2, secondValue2)}");            
+
+            //Call Multiplication           
+            Console.Write("Input first value: ");
+            double firstValue3 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Input second value: ");
+            double secondValue3 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine($"Value = {calc.Multiplication(firstValue3, secondValue3)}");            
+
+            //Call Division           
+            Console.Write("Input first value: ");
+            double firstValue4 = double.Parse(Console.ReadLine());
+            Console.Write("Input second value: ");
+            double secondValue4 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Value = {calc.Division(firstValue4, secondValue4)}");            
+
+            //Call Remainder            
+            Console.Write("Input first value: ");
+            double firstValue5 = double.Parse(Console.ReadLine());
+            Console.Write("Input second value: ");
+            double secondValue5 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Remainder = {calc.Remainder(firstValue5, secondValue5)}");           
+
+            //Call AreaOfCircle            
+            Console.Write("Input radius: ");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Area of circle = {calc.AreaOfCircle(radius)}");          
+        }
+    }
+}
